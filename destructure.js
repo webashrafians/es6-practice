52b90cc44eb1e0226605136c874f741a6b8d92bb
webashrafians <webashrafians@gmail.com>
2020-08-07 17:26:55 +0600

    test

---
+++ destructure.js
@@ -0,0 +1,18 @@
+const person = {
+    name : "jack William",
+    age : 17,
+    job : "facebooker",
+    gfName : "Ema Watson",
+    address : "Kochu Khet",
+    phone: '01701643404',
+}
+
+const {phone, gfName, address} = person;
+// const gfName = person.gfName;
+
+// const gfName = person.gfName;
+// const phone = person.phone;
+
+console.log(gfName, phone, address);
+console.log(gfName, phone);
+console.log(gfName, phone);
